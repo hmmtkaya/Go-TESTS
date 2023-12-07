9ce3930cba264dbe284bcefea50955dcce62046c
hmmtkaya <hmmtkaya@gmail.com>
2023-12-07 10:50:40 +0000

    commit 1

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		firstname, lastname string = "Himmet", "Kaya"
+
+		age       int  = 33
+		isMarried bool = true
+		name2          = "hmmt"
+		name3          = "Sibel"
+	)
+
+	fmt.Println(firstname, lastname, name2, name3)
+	fmt.Println(age)
+	fmt.Println(isMarried)
+
+}
